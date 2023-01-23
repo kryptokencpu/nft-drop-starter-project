e4cde8609fd6eda6f4bc61f5fdd81125b90bc446
codescrubb <116120394+kryptokencpu@users.noreply.github.com>
2023-01-23 10:00:37 -0500

    Progress #01

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,16 +1,30 @@
 import React from "react";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+
 
 // Constants
-const TWITTER_HANDLE = "_buildspace";
+const TWITTER_HANDLE = "KryptoKenCPU";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const Home = () => {
+
+    const renderNotConnectedContainer = () => (
+        <div>
+            <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji" />
+
+            <div className="button-container">
+                <WalletMultiButton className="cta-button connect-wallet-button"/>
+            </div>
+        </div>
+    );
+
     return (
         <div className="App">
             <div className="container">
                 <div className="header-container">
                     <p className="header">🍭 Candy Drop</p>
                     <p className="sub-text">NFT drop machine with fair mint</p>
+                    {renderNotConnectedContainer()}
                 </div>
                 <div className="footer-container">
                     <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
